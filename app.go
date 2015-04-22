aad4fdf6246843ac0982a4c80b9267b9461db07f
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2015-04-22 09:08:39 -0700

    parse json to extract languages_url from repositories

--- app.go
+++ app.go
@@ -1,39 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"net/http"
-	"os"
 )
 
+type Repo struct {
+	LanguagesUrl string `json:"languages_url"`
+}
+
+func getJSONArray(url string) ([]Repo, error) {
+	var response_map []Repo
+
+	response, err := http.Get(url)
+	if err != nil {
+		return response_map, err
+	}
+	defer response.Body.Close()
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return response_map, err
+	}
+	err = json.Unmarshal([]byte(data), &response_map)
+	return response_map, err
+}
+
 func github_badger(username string) map[string]int {
-	token := os.Getenv("GITHUB_TOKEN")
+	// token := os.Getenv("GITHUB_TOKEN")
 	languages := make(map[string]int)
 
-	response, err := http.Get("https://api.github.com/users/" + username + "/repos?access_token=" + token)
+	repos, err := getJSONArray("https://api.github.com/users/" + username + "/repos")
 	if err != nil {
 		panic(err)
 	}
-	defer response.Body.Close()
-	if response.StatusCode == 200 {
-		content, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(string(content))
+	for _, repo := range repos {
+		fmt.Println(repo)
 	}
 
 	return languages
 }
 
 func main() {
-	r := gin.Default()
-	r.GET("/github/:username", func(c *gin.Context) {
-		github_badger(c.Params.ByName("username"))
-		c.String(http.StatusOK, "OK")
-	})
+	github_badger("3zcurdia")
+	// r := gin.Default()
+	// r.GET("/github/:username", func(c *gin.Context) {
+	// 	github_badger(c.Params.ByName("username"))
+	// 	c.String(http.StatusOK, "OK")
+	// })
 
-	r.Run(":" + os.Getenv("PORT"))
+	// r.Run(":" + os.Getenv("PORT"))
 }
