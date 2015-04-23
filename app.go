6c9dc1d7eb65c3abad7e83ea18cd5ad84b492e0d
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2015-04-23 00:43:29 -0700

    It worked! fetch language statistics from github

--- app.go
+++ app.go
@@ -2,54 +2,79 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 type Repo struct {
 	LanguagesUrl string `json:"languages_url"`
 }
 
-func getJSONArray(url string) ([]Repo, error) {
-	var response_map []Repo
-
-	response, err := http.Get(url)
+func getREST(url string) ([]byte, error) {
+	http_response, err := http.Get(url)
 	if err != nil {
-		return response_map, err
+		return nil, err
 	}
-	defer response.Body.Close()
+	defer http_response.Body.Close()
 
-	data, err := ioutil.ReadAll(response.Body)
+	data, err := ioutil.ReadAll(http_response.Body)
 	if err != nil {
-		return response_map, err
+		return nil, err
 	}
-	err = json.Unmarshal([]byte(data), &response_map)
-	return response_map, err
+	return []byte(data), nil
 }
 
-func github_badger(username string) map[string]int {
-	// token := os.Getenv("GITHUB_TOKEN")
+func getHashNumber(url string) (map[string]int, error) {
+	var hash map[string]int
+	res, err := getREST(url)
+
+	err = json.Unmarshal(res, &hash)
+	return hash, err
+}
+
+func getArray(url string) ([]Repo, error) {
+	var array []Repo
+	res, err := getREST(url)
+
+	err = json.Unmarshal(res, &array)
+	return array, err
+}
+
+func badger(username string) map[string]int {
+	token := "access_token=" + os.Getenv("GITHUB_TOKEN")
 	languages := make(map[string]int)
 
-	repos, err := getJSONArray("https://api.github.com/users/" + username + "/repos")
+	repos, err := getArray("https://api.github.com/users/" + username + "/repos" + "?" + token)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 	for _, repo := range repos {
-		fmt.Println(repo)
+		hash, err := getHashNumber(repo.LanguagesUrl + "?" + token)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		for key, value := range hash {
+			_, ok := languages[key]
+			if ok {
+				languages[key] += value
+			} else {
+				languages[key] = value
+			}
+		}
 	}
 
 	return languages
 }
 
 func main() {
-	github_badger("3zcurdia")
-	// r := gin.Default()
-	// r.GET("/github/:username", func(c *gin.Context) {
-	// 	github_badger(c.Params.ByName("username"))
-	// 	c.String(http.StatusOK, "OK")
-	// })
+	r := gin.Default()
+	r.GET("/languages/:username", func(c *gin.Context) {
+		res := badger(c.Params.ByName("username"))
+		c.JSON(http.StatusOK, res)
+	})
 
-	// r.Run(":" + os.Getenv("PORT"))
+	r.Run(":" + os.Getenv("PORT"))
 }
