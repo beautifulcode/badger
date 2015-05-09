ea8dddeb1da8160c7347da886ab4dfc770d62254
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2015-05-08 19:59:37 -0700

    improve speed with concurrency

--- app.go
+++ app.go
@@ -1,3 +1,4 @@
+// +build !debug
 package main
 
 import (
@@ -8,13 +9,36 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 type Repo struct {
 	LanguagesUrl string `json:"languages_url"`
 }
 
-func getREST(url string) ([]byte, error) {
+func asyncGet(url string, response chan []byte, errorChannel chan error) {
+	// start := time.Now()
+	http_response, err := http.Get(url)
+	// elapsed := time.Since(start)
+	// fmt.Println("Get: ", elapsed)
+	if err != nil {
+		errorChannel <- err
+		return
+	}
+	defer http_response.Body.Close()
+	if http_response.StatusCode != 200 {
+		errorChannel <- errors.New("API response:" + http_response.Status + " url:" + url)
+		return
+	}
+	data, err := ioutil.ReadAll(http_response.Body)
+	if err != nil {
+		errorChannel <- err
+		return
+	}
+	response <- []byte(data)
+}
+
+func Get(url string) ([]byte, error) {
 	http_response, err := http.Get(url)
 	if err != nil {
 		return nil, err
@@ -30,49 +54,66 @@ func getREST(url string) ([]byte, error) {
 	return []byte(data), nil
 }
 
-func getHashNumber(url string) (map[string]int, error) {
+func parseMapInt(data []byte) map[string]int {
 	var hash map[string]int
-	res, err := getREST(url)
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(res, &hash)
-	return hash, nil
-}
-
-func getArray(url string) ([]Repo, error) {
-	var array []Repo
-	res, err := getREST(url)
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(res, &array)
-	return array, nil
-}
-
-func badger(username string) map[string]int {
-	token := "access_token=" + os.Getenv("GITHUB_TOKEN")
-	languages := make(map[string]int)
-
-	repos, err := getArray("https://api.github.com/users/" + username + "/repos" + "?" + token)
+	err := json.Unmarshal(data, &hash)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	return hash
+}
+
+func parseRepo(data []byte) []Repo {
+	var array []Repo
+	err := json.Unmarshal(data, &array)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return array
+}
+
+func repoCount(username string) map[string]int {
+	token := os.Getenv("GITHUB_TOKEN")
+
+	errChan := make(chan error)
+	response := make(chan []byte)
+
+	languages := make(map[string]int)
+
+	res, err := Get("https://api.github.com/users/" + username + "/repos" + "?" + token)
+	if err != nil {
+		log.Fatalln(err)
+		return languages
+	}
+	repos := parseRepo(res)
+	reposLeft := len(repos)
+
 	for _, repo := range repos {
-		hash, err := getHashNumber(repo.LanguagesUrl + "?" + token)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		for key, value := range hash {
-			_, ok := languages[key]
-			if ok {
-				languages[key] += value
-			} else {
-				languages[key] = value
-			}
-		}
+		go asyncGet(repo.LanguagesUrl+"?"+token, response, errChan)
 	}
 
+	for {
+		select {
+		case res := <-response:
+			hash := parseMapInt(res)
+			for key, value := range hash {
+				_, ok := languages[key]
+				if ok {
+					languages[key] += value
+				} else {
+					languages[key] = value
+				}
+			}
+			reposLeft--
+		case err := <-errChan:
+			log.Fatalln(err)
+		case <-time.After(1000 * time.Millisecond):
+			log.Fatalln("Timeout")
+		}
+		if reposLeft == 0 {
+			break
+		}
+	}
 	return languages
 }
 
@@ -84,7 +125,7 @@ func main() {
 	})
 
 	r.GET("/languages/:username", func(c *gin.Context) {
-		res := badger(c.Params.ByName("username"))
+		res := repoCount(c.Params.ByName("username"))
 		c.JSON(http.StatusOK, res)
 	})
 
