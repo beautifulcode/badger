c238d7193a887be1ee7d611c92f008c54a814172
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2015-05-10 23:23:30 -0700

    set cache headers

--- app.go
+++ app.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"github.com/3zcurdia/merithub/webhooks"
 	"github.com/codegangsta/negroni"
 	"github.com/julienschmidt/httprouter"
@@ -10,14 +12,23 @@ import (
 	"os"
 )
 
+func getSha1(s []byte) []byte {
+	h := sha1.New()
+	h.Write(s)
+	return h.Sum(nil)
+}
+
 func main() {
 	r := render.New()
 	mux := httprouter.New()
 	mux.GET("/", func(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-		w.Write([]byte("Go to /languages/:username"))
+		w.Write([]byte("Go to /github/:username/languages/"))
 	})
 
-	mux.GET("/languages/:username", func(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	mux.GET("/github/:username/languages/", func(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+		etag := getSha1([]byte(ps.ByName("username")))
+		w.Header().Set("Cache-Control", "public, max-age=86400")
+		w.Header().Set("Etag", hex.EncodeToString(etag))
 		res := webhooks.GithubCount(ps.ByName("username"))
 		r.JSON(w, http.StatusOK, res)
 	})
